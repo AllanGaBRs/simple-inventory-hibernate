615a0de22f3da998f58c0d1cd45530cf696dbc0d
allangabr <allanmediogames@gmail.com>
2025-05-16 23:03:29 -0300

    feat: hibernate configuration

---
+++ src/main/java/com/trabalho/pelegrin/hibernate/HibernateApplication.java
@@ -0,0 +1,25 @@
+package com.trabalho.pelegrin.hibernate;
+
+import model.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateApplication {
+
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setName("allanfdb");
+		p.setEmail("allan@gmail");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("TRABALHO_HIBERNATE");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
+		System.out.println("commitado");
+
+	}
+}
